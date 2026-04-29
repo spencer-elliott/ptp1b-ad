19d772c21ce6be914d94700cf0c7f66465e04a94
Spencer Elliott <spencer.e.elliott@gmail.com>
2026-04-29 11:27:05 -0400

    Continue writing Jupyter notebook documentation

--- ptp1b_quantification.sh
+++ ptp1b_quantification.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Activate before running: conda activate kallisto_env
+# Downloads needed: Reference transcriptome and 10xv3 whitelist. See corresponding Jupyter notebook for instructions
 
 # Create an index for kallisto from the mouse reference transcriptome:
 kallisto index -i mm10.idx gencode.vM23.transcripts.fa.gz
@@ -11,8 +12,9 @@ kallisto bus -i mm10.idx -o kallisto_output -x 10xv3 -t 4 SRR36691286_1.fastq.gz
 bustools sort -t 4 -o kallisto_output/sorted.bus kallisto_output/output.bus
 
 # Fixing sequencing errors (needs whitelist)
-bustools correct -w 3M-february-2018.txt -o /dev/stdout kallisto_output/sorted.bus | \
-  bustools sort -t 4 -o kallisto_output/corrected_sorted.bus -
+bustools correct -w 3M-february-2018.txt -o kallisto_output/corrected.bus kallisto_output/sorted.bus
+# Re-sorting after correction
+bustools sort -t 4 -o kallisto_output/corrected_sorted.bus kallisto_output/corrected.bus
 
 # Creating a gene expression matrix - gives us gene expression counts per cell
 bustools count \
