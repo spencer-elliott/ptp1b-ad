064b51878a58f7f0beea2f37d796a342b035c4e4
Spencer Elliott <spencer.e.elliott@gmail.com>
2026-05-14 15:53:15 -0400

    Fix Jupyter notebooks with new file paths

--- scripts/ptp1b_fastQC.sh
+++ scripts/ptp1b_fastQC.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # Activate before running: conda activate fastqc_env
 
+# Create an output directory for fastqc
+mkdir -p data/fastqc
+
 # Run fastqc to see the quality of raw data/reads
-fastqc SRR36691286_1.fastq.gz SRR36691286_2.fastq.gz
+fastqc data/fastq/SRR36691286_1.fastq.gz data/fastq/SRR36691286_2.fastq.gz -o data/fastqc
--- scripts/ptp1b_quantification.sh
+++ scripts/ptp1b_quantification.sh
@@ -3,26 +3,44 @@
 # Files needed: Reference transcriptome, 10xv3 whitelist, transcripts.txt and transcripts_to_genes.txt 
 #   See corresponding Jupyter notebook for instructions
 
+# Create directories for reference files and kallisto output
+mkdir -p data/references
+mkdir -p results/kallisto_output
+
 # Create an index for kallisto from the mouse reference transcriptome:
-kallisto index -i mm10.idx gencode.vM23.transcripts.fa.gz
+kallisto index -i data/references/mm10.idx \
+  data/references/gencode.vM23.transcripts.fa.gz
 
 # Pseudoalignment of reads (for 10x data)
-kallisto bus -i mm10.idx -o kallisto_output -x 10xv3 -t 4 SRR36691286_1.fastq.gz SRR36691286_2.fastq.gz
+kallisto bus \
+  -i data/references/mm10.idx \
+  -o results/kallisto_output \
+  -x 10xv3 \
+  -t 4 \
+  data/fastq/SRR36691286_1.fastq.gz data/fastq/SRR36691286_2.fastq.gz
 
 # Ordering/sorting the barcodes
-bustools sort -t 4 -o kallisto_output/sorted.bus kallisto_output/output.bus
+bustools sort -t 4 \
+  -o results/kallisto_output/sorted.bus \
+  results/kallisto_output/output.bus
 
 # Fixing sequencing errors (needs whitelist)
-bustools correct -w 3M-february-2018.txt -o kallisto_output/corrected.bus kallisto_output/sorted.bus
+bustools correct \
+  -w data/references/3M-february-2018.txt \
+  -o results/kallisto_output/corrected.bus \
+  results/kallisto_output/sorted.bus
 
 # Re-sorting after correction
-bustools sort -t 4 -o kallisto_output/corrected_sorted.bus kallisto_output/corrected.bus
+bustools sort \
+  -t 4 \
+  -o results/kallisto_output/corrected_sorted.bus \
+  results/kallisto_output/corrected.bus
 
 # Creating a gene expression matrix - gives us gene expression counts per cell
 bustools count \
-  -o kallisto_output/counts \
-  -g kallisto_output/transcripts_to_genes.txt \
-  -e kallisto_output/matrix.ec \
-  -t kallisto_output/transcripts.txt \
+  -o results/kallisto_output/counts \
+  -g results/kallisto_output/transcripts_to_genes.txt \
+  -e results/kallisto_output/matrix.ec \
+  -t results/kallisto_output/transcripts.txt \
   --genecounts \
-  kallisto_output/corrected_sorted.bus
+  results/kallisto_output/corrected_sorted.bus
